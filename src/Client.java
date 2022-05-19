9ce25f942a2cffb14e16f438eec70fb4d0db500c
TheCotolett <lorenzotamburrini2@gmail.com>
2022-05-19 15:57:18 +0200

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        //La classe Socket è l'implementazione delle socket tcp
+        //Quando si instanzia bisogna specificare l'indirizzo e la porta
+        Socket socket = new Socket("localhost", 4999);
+
+        //PrintWriter è la classe per la stampa di una stream, in questo caso
+        //di una stream di output della socket
+        PrintWriter pr = new PrintWriter(socket.getOutputStream());
+
+        //Invio del messaggio al server attraverso la socket
+        pr.println("Client connesso");
+        pr.flush(); //Svuota il flusso di output e forza la scrittura dei byte memorizzati nel buffer
+
+        //Instanziazione di InputStreamReader e BufferedReader
+        //con l'input dalla socket
+        InputStreamReader in = new InputStreamReader(socket.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        //Con il BufferedReader legge la riga ricevuta in tcp dal server
+        //e la stampa
+        String msg = bf.readLine();
+        System.out.println("SERVER: "+msg);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        //ServerSocket è l'implementazione dei socket tcp del server, che aspetta
+        //di ricevere una richiesta di connessione
+        ServerSocket serverSocket = new ServerSocket(4999);
+
+        //Loop per l'esecuzione continua del programma
+        do {
+            //Ottenere data e orario correnti
+            Date date = Calendar.getInstance().getTime();
+            //Formattare data nel formato anno/mese/giorno ora/minuto/secodni
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+            //conversione da date a String
+            String dataString = dateFormat.format(date);
+
+            //Ascolto per una connessione sulla serverSocket e accettazione
+            Socket socket = serverSocket.accept();
+
+            //InputStreamReader collegato alla socket per ricevere input dal client
+            InputStreamReader in = new InputStreamReader(socket.getInputStream());
+            BufferedReader bf = new BufferedReader(in);
+
+            //Ricezione input dal client e stampa
+            String msg = bf.readLine();
+            System.out.println("CLIENT: " + msg);
+
+            //PrintWriter è la classe per la stampa di una stream, in questo caso
+            //di una stream di output della socket
+            PrintWriter pr = new PrintWriter(socket.getOutputStream());
+            //Invio della data in formato stringa
+            pr.println(dataString); //Svuota il flusso di output e forza la scrittura dei byte memorizzati nel buffer
+            pr.flush();
+        } while (true);
+    }
+}
